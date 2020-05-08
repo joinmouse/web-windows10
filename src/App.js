54c8effef383bfcd4e5d9b27dfeb345cf62edd90
joinmouse <908380434@qq.com>
2020-05-08 17:54:51 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './App.css';
+
+import computer from './assets/computer.jpg'
+
+function App() {
+
+    return (
+        <div className="App">
+            {/* <div ondrop="drop(event)" ondragover="allowDrop(event)">
+                <img src={computer} draggable="true" ondragstart="drag(event)" id="drag1" />
+            </div>
+            <div ondrop="drop(event)" ondragover="allowDrop(event)"></div> */}
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default App
